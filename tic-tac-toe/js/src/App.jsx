96fd770ba7520324a3b076fa2d78b2ec9147d335
shisotem <129283841+shisotem@users.noreply.github.com>
2023-10-18 07:13:43 +0900

    feat: クリックイベントに応じて、数字をコンソール出力するよう変更

--- tic-tac-toe/js/src/App.jsx
+++ tic-tac-toe/js/src/App.jsx
@@ -1,5 +1,12 @@
 function Square({ value }) {
-  return <button className="square">{value}</button>;
+  function handleClick() {
+    console.log(value);
+  }
+  return (
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
+  );
 }
 
 export default function Board() {
